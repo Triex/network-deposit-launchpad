2ed78d0d78b7a9b78a9916301e946edae982b026
Reto Ryter <reto@twy.gmbh>
2021-11-03 15:29:46 +0000

    chore(content): validator instructions

--- src/pages/GenerateKeys/HowToStartValidator.tsx
+++ src/pages/GenerateKeys/HowToStartValidator.tsx
@@ -20,14 +20,17 @@ export const HowToStartValidator = () => {
             <span style={{ color: colors.red.medium }}>
               # Linux
               <br />$ lukso start --validate --node-name
-              "REPLACE-WITH-NODE-NAME"
+              "REPLACE-WITH-NODE-NAME" --coinbase YOUR_ETH1_ADDRESS
               <br />
               <br />
               # MacOS
               <br />$ sudo lukso start --validate --node-name
-              "REPLACE-WITH-NODE-NAME"
+              "REPLACE-WITH-NODE-NAME" --coinbase YOUR_ETH1_ADDRESS
             </span>
           </Pre>
+        </Alert>
+        <Alert variant="info" className="my40">
+          <FormattedMessage defaultMessage="Replace YOUR_ETH1_ADDRESS with an address you own the private key of. This address will receive the transaction fees of blocks that the validator client has produced." />
         </Alert>
       </ul>
     </div>
